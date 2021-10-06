3f5300e1bf52512c4d3202ce91b8f35b9456f0cb
kali <kali@local>
2021-10-06 10:03:43 +0300

    Minor fixes

--- cryptopals/set1/main.go
+++ cryptopals/set1/main.go
@@ -10,16 +10,21 @@ func to_hex(input []byte) string {
 	return hex.EncodeToString(input)
 }
 
-func hex_to_base64(hexstring string) string{
-	decodehex, _ := hex.DecodeString(hexstring)
+func from_str(input string) ([]byte, error) {
+	return hex.DecodeString(input)
+}
+
+func hex_to_base64(input string) string{
+	decodehex, _ := from_str(input)
 	enc64 := base64.StdEncoding.EncodeToString(decodehex)
 	return enc64
 
 }
 
-func fixed_xor(input, second string) []byte {
-	decodeinput, _ := hex.DecodeString(input)
-	decodesecond, _ := hex.DecodeString(second)
+func fixed_xor(param1, param2 string) []byte {
+
+	decodeinput, _ := from_str(param1)
+	decodesecond, _ := from_str(param2)
 	output := make([] byte, 0)
 
 	for i := range decodeinput {
@@ -30,8 +35,7 @@ func fixed_xor(input, second string) []byte {
 
 func main(){
 	hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-	res := fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
-	fmt.Println("%s", to_hex(res))
+	fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
 }
 
 
