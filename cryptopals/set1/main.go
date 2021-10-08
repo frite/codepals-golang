1add4f759aadeb56614e92d8d4ac97a92ffba642
kali <kali@local>
2021-10-08 13:33:32 +0300

    Still needs work

--- cryptopals/set1/main.go
+++ cryptopals/set1/main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 )
+
+/*
+* From http://www.data-compression.com/english.html
+*  This
+* http://practicalcryptography.com/cryptanalysis/letter-frequencies-various-languages/english-letter-frequencies/
+* may be more useful and is more rich in data, trigrams etc.
+*/
+var LETTER_FREQUENCY = map[byte]float64 {
+    'a': 0.0651738,
+    'b': 0.0124248,
+    'c': 0.0217339,
+    'd': 0.0349835,
+    'e': 0.1041442,
+    'f': 0.0197881,
+    'g': 0.0158610,
+    'h': 0.0492888,
+    'i': 0.0558094,
+    'j': 0.0009033,
+    'k': 0.0050529,
+    'l': 0.0331490,
+    'm': 0.0202124,
+    'n': 0.0564513,
+    'o': 0.0596302,
+    'p': 0.0137645,
+    'q': 0.0008606,
+    'r': 0.0497563,
+    's': 0.0515760,
+    't': 0.0729357,
+    'u': 0.0225134,
+    'v': 0.0082903,
+    'w': 0.0171272,
+    'x': 0.0013692,
+    'y': 0.0145984,
+    'z': 0.0007836,
+    ' ': 0.1918182,
+}
 
 func to_hex(input []byte) string {
 	return hex.EncodeToString(input)
@@ -25,7 +63,7 @@ func fixed_xor(param1, param2 string) []byte {
 
 	decodeinput, _ := from_str(param1)
 	decodesecond, _ := from_str(param2)
-	output := make([] byte, 0)
+	output := make([]byte, 0)
 
 	for i := range decodeinput {
 		output = append(output, decodeinput[i] ^ decodesecond[i])
@@ -33,9 +71,54 @@ func fixed_xor(param1, param2 string) []byte {
 	return output
 }
 
+func xor(input, key []byte){
+	for i := 0; i < len(key); i += 1 {
+		input[i] ^= key[i]
+	}
+
+}
+
+func calcEtaoinShrdlu(input []bytes) float64 {
+	// https://en.wikipedia.org/wiki/Etaoin_shrdlu
+	size = len(input)
+	var score float64
+	for i := 1; i < size; i += 1;{
+		
+	}
+}
+
+func single_byte_xor(input []byte) ([]byte, byte, float64){
+	size := len(input)
+	/*
+	* WHY ON EARTH IS THIS NOT WORKING?
+	results := make(map[byte][]byte)
+	for i := 0; i< 256; i += 1 {
+		results[i], _ := xor(input, bytes.Repeat([]byte{byte(i)}, size))
+	}
+	*/
+	var score float64
+	var plaintext []byte
+	var key byte
+	for i := 0; i < 256; i++ {
+		tempResult := make([]byte, len(input))
+		copy(tempResult, buf)
+		xor(tempResult, bytes.Repeat([]byte{byte(i)}, size))
+		tempScore := calcEtaoinShrdlu(tempResult)
+		if tempScore > score {
+			score = tempScore
+			plaintext = tempResult
+			key = byte(i)
+		}
+	}
+	return plaintext, key, score
+
+}
+
 func main(){
 	hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 	fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+	cipher, _ := from_str("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	single_byte_xor(cipher)
 }
 
 
